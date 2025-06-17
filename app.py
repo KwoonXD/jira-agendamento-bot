bd106b5e39ed6a97b905f3039721801e3ef65180
KwoonXD <160345147+KwoonXD@users.noreply.github.com>
2025-06-17 13:49:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,17 +31,20 @@ def gerar_mensagem(loja, chamados):
     for ch in chamados:
         data_agendada = ch.get('data_agendada')
         data_formatada = datetime.strptime(data_agendada, "%Y-%m-%dT%H:%M:%S.%f%z").strftime('%d/%m/%Y %H:%M') if data_agendada else '--'
-        blocos.append(f"*{ch['key']}*
-*Loja:* {loja}
-*PDV:* {ch['pdv']}
-*ATIVO:* {ch['ativo']}
-*Problema:* {ch['problema']}
-*Data Agendada:* {data_formatada}
-*****")
-    blocos.append(f"*Endereço:* {chamados[0]['endereco']}
-*Estado:* {chamados[0]['estado']}
-*CEP:* {chamados[0]['cep']}
-*Cidade:* {chamados[0]['cidade']}")
+        blocos.append(
+            f"*{ch['key']}*\n"
+            f"*Loja:* {loja}\n"
+            f"*PDV:* {ch['pdv']}\n"
+            f"*ATIVO:* {ch['ativo']}\n"
+            f"*Problema:* {ch['problema']}\n"
+            f"*Data Agendada:* {data_formatada}\n*****"
+        )
+    blocos.append(
+        f"*Endereço:* {chamados[0]['endereco']}\n"
+        f"*Estado:* {chamados[0]['estado']}\n"
+        f"*CEP:* {chamados[0]['cep']}\n"
+        f"*Cidade:* {chamados[0]['cidade']}"
+    )
     return "\n".join(blocos)
 
 def transicionar_status(issue_key, id_transicao):
