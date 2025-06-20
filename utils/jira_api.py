7755e09b0676fbb86b485fea9df9235108c8665b
KwoonXD <160345147+KwoonXD@users.noreply.github.com>
2025-06-20 09:57:02 -0300

    Update jira_api.py

--- utils/jira_api.py
+++ utils/jira_api.py
@@ -17,20 +17,25 @@ class JiraAPI:
         res = requests.get(f"{self.jira_url}/rest/api/3/search", headers=self.headers, auth=self.auth, params=params)
         return res.json().get("issues", []) if res.status_code == 200 else []
 
-    def agrupar_chamados(self, chamados):
-        agrupado = defaultdict(list)
-        for issue in chamados:
-            fields = issue["fields"]
-            loja = fields.get("customfield_14954", {}).get("value", "Loja Desconhecida")
-            agrupado[loja].append({
-                "key": issue["key"],
-                "pdv": fields.get("customfield_14829", "--"),
-                "ativo": fields.get("customfield_14825", {}).get("value", "--"),
-                "problema": fields.get("customfield_12374", "--"),
-                "endereco": fields.get("customfield_12271", "--"),
-                "estado": fields.get("customfield_11948", {}).get("value", "--"),
-                "cep": fields.get("customfield_11993", "--"),
-                "cidade": fields.get("customfield_11994", "--"),
-                "data_agendada": fields.get("customfield_12036", "")
-            })
-        return agrupado
+   def agrupar_chamados(self, chamados):
+    agrupado = defaultdict(list)
+    for issue in chamados:
+        fields = issue["fields"]
+        loja = fields.get("customfield_14954", {}).get("value", "Loja Desconhecida")
+        ativo = fields.get("customfield_14825", {}).get("value", "--")
+        problema = fields.get("customfield_12374", "--")
+        pdv = fields.get("customfield_14829", "--")
+
+        agrupado[loja].append({
+            "key": issue["key"],
+            "pdv": pdv,
+            "ativo": ativo,
+            "problema": problema,
+            "endereco": fields.get("customfield_12271", "--"),
+            "estado": fields.get("customfield_11948", {}).get("value", "--"),
+            "cep": fields.get("customfield_11993", "--"),
+            "cidade": fields.get("customfield_11994", "--"),
+            "data_agendada": fields.get("customfield_12036", "")
+        })
+    return agrupado
+
